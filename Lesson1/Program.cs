bc5d0306e35131202af7cd58fbce07cc5f99ce8c
Usman Rahat <engr.usmanrahat93@gmail.com>
2023-06-27 22:05:56 +0930

    added "public" access modifer to Main method.

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson1
     internal class Program
     {
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             DelPointer del = new DelPointer(Greeting);
 
