b5d5725ca0f09d98f39fb27b7612b203f2c8cab3
Usman Rahat <engr.usmanrahat93@gmail.com>
2023-06-28 23:36:50 +0930

    Lesson 2 added.

---
+++ Lesson2/Developer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson2
+{
+    public class Developer
+    {
+        public delegate bool IsPromote(Developer developer);
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+        public int NumberOfClients { get; set; }
+
+        public static void PromoteDeveloper(List<Developer> developerList, IsPromote ShallPromote)
+        {
+            foreach (Developer developer in developerList) 
+            {
+                if (ShallPromote(developer))
+                {
+                    Console.WriteLine($"{developer.Name} is promoted");
+                }
+            }
+
+        }
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Lesson2;
+using static Lesson2.Developer;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Developer> developers= new List<Developer>();
+            developers.Add(new Developer() { ID = 1, Name = "Usman Rahat", Salary = 100, Experience = 3, NumberOfClients = 7  });
+            developers.Add(new Developer() { ID = 1, Name = "John Adam", Salary = 150, Experience = 13, NumberOfClients = 20 });
+
+            IsPromote isPromotable = new IsPromote(Promote);
+
+            Developer.PromoteDeveloper(developers, isPromotable);
+
+            Console.ReadLine();
+        }
+
+        public static bool Promote(Developer developer) 
+        {
+            return (developer.Experience > 2 && developer.Salary > 90);
+        }
+    }
+}
